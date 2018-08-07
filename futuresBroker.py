218fdfa9e841c71b700e7a4ef386bf8116a2066a
Martin Domaracky <martin.domaracky@desy.de>
2018-08-07 17:35:35 +0200

    update

--- futuresBroker.py
+++ futuresBroker.py
@@ -8,18 +8,17 @@ BUY = 1
 
 
 class FuturesBroker(Broker):
-    dealTreshold = 0.1
+    dealTreshold = 0.3
     dealCoeficient = 0.7
     orderPosted = False
     ordersCount = 0
 
     def onRateDiff(self, product, diff, currentRates):
         if self.dealingFlag.isSet(): return
-
+        print(diff)
         if abs(diff) > self.dealTreshold:
 
             ordersCount = len(self.get_orders()[0])
-            print(ordersCount, self.ordersCount)
             if ordersCount != self.ordersCount: self.initBroker()
             if ordersCount > 0: return
 
@@ -38,24 +37,36 @@ class FuturesBroker(Broker):
             "cancel_after": "5 min",
             "post_only": True
         }
-        if diff > 0 and baseBalance[1] > 0:
-            dealParameters["size"] = round(baseBalance[1] * self.dealCoeficient, 8)
-            if float(self.products[product]["min"]) < dealParameters["size"]:
-                self.dealingFlag.clear()
-                return
 
-            dealParameters["price"] += float(self.products[product]["increment"])
+        if diff > 0 and baseBalance[1] > 0:
+            # SELL
+
+            dealParameters["size"] = round(baseBalance[1] * self.dealCoeficient, 8)
+            if float(self.products[product]["min"]) > dealParameters["size"]:
+                if baseBalance[1] < float(self.products[product]["min"]):
+                    self.dealingFlag.clear()
+                    return
+                else:
+                    dealParameters["size"] = float(self.products[product]["min"])
+
+            dealParameters["price"] += 2*float(self.products[product]["increment"])
             dealParameters["price"] = round(dealParameters["price"], abs(
                 decimal.Decimal(self.products[product]["increment"]).as_tuple().exponent))
             dealParameters["side"] = "sell"
             deal = self.sell(**dealParameters)
-        elif diff < 0 and quoteBalance[1] > 0:
-            dealParameters["size"] = round((quoteBalance[1] / currentRates[0]) * self.dealCoeficient, 8)
-            if float(self.products[product]["min"]) < dealParameters["size"]:
-                self.dealingFlag.clear()
-                return
 
-            dealParameters["price"] -= float(self.products[product]["increment"])
+        elif diff < 0 and quoteBalance[1] > 0:
+            # BUY
+
+            dealParameters["size"] = round((quoteBalance[1] / currentRates[0]) * self.dealCoeficient, 8)
+            if float(self.products[product]["min"]) > dealParameters["size"]:
+                if baseBalance[1] < float(self.products[product]["min"]) * currentRates[0]:
+                    self.dealingFlag.clear()
+                    return
+                else:
+                    dealParameters["size"] = float(self.products[product]["min"])
+
+            dealParameters["price"] -= 2*float(self.products[product]["increment"])
             dealParameters["price"] = round(dealParameters["price"], abs(
                 decimal.Decimal(self.products[product]["increment"]).as_tuple().exponent))
             dealParameters["side"] = "buy"
